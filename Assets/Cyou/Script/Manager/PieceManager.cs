08dc1c1f4bded9763313bcdda494529ea4fdd46c
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2025-11-05 20:11:41 +0900

    9
    修正攻击问题

--- Assets/Cyou/Script/Manager/PieceManager.cs
+++ Assets/Cyou/Script/Manager/PieceManager.cs
@@ -988,7 +988,7 @@ public class PieceManager : MonoBehaviour
             return null;
         }
 
-        if (!pieces.TryGetValue(targetID, out Piece target))
+        if (!enemyPieces.TryGetValue(targetID, out Piece target))
         {
             Debug.LogError($"ターゲットが見つかりません: ID={targetID}");
             return null;
