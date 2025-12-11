27d84652279c9ba2e140d985d5b464e3345efa9a
sillyyyy1003 <guoning1003@gmail.com>
2025-12-11 14:07:03 +0900

    no message

--- Assets/GuoNing/Scripts/UI/HexButton/HexButton.cs
+++ Assets/GuoNing/Scripts/UI/HexButton/HexButton.cs
@@ -9,7 +9,7 @@ public class HexButton : MonoBehaviour
 	private HexButtonCollider Collider;
 	private TextMeshProUGUI text;
 	private Image background;
-	private Image shadow;
+	//private Image shadow;
 	private Toggle toggle;
 	
 
@@ -39,7 +39,7 @@ public class HexButton : MonoBehaviour
 	{
 		Collider = GetComponentInChildren<HexButtonCollider>();
 		text = GetComponentInChildren<TextMeshProUGUI>();
-		shadow = transform.Find("Shadow")?.GetComponent<Image>();
+		//shadow = transform.Find("Shadow")?.GetComponent<Image>();
 		background = transform.Find("Background")?.GetComponent<Image>();
 		toggle = GetComponent<Toggle>();
 
@@ -103,7 +103,7 @@ public class HexButton : MonoBehaviour
 		if (inside && mouseDown && !isPressed)
 		{
 			isPressed = true;
-			shadow.gameObject.SetActive(false);
+			background.GetComponent<Shadow>().gameObject.SetActive(false);
 			UpdateVisual(pressedColor);
 			UpdateTextVisual(pressedTextColor);
 		}
@@ -113,7 +113,7 @@ public class HexButton : MonoBehaviour
 			// 点击确认
 			if (inside)
 			{
-				shadow.gameObject.SetActive(true);  // 启用阴影
+				background.GetComponent<Shadow>().gameObject.SetActive(true);  // 启用阴影
 				// 如果是切换状态
 				if (isToggle)
 				{
