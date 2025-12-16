7248dbf272f6d2e1a593f9cf8bf606e73382f4de
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2025-12-16 19:36:25 +0900

    添加疯狂教

--- Assets/Ri/Scripts/GameManager/SceneStateManager.cs
+++ Assets/Ri/Scripts/GameManager/SceneStateManager.cs
@@ -170,7 +170,7 @@ public class SceneStateManager : MonoBehaviour
     private void ChooseRandomReligion()
     {
         // 生成一个随机值（目前为1~4 之后扩展到1~8）
-        int religion = Random.Range(1, 4);
+        int religion = Random.Range(1, 5);
         PlayerReligion = (Religion)religion;
 
     }
